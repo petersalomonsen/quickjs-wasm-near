a597e083ad6b13ce17cdb44c5a22edc8899b0062
Peter Salomonsen <pjsalomonsen@gmail.com>
2022-04-27 21:37:13 +0200

    test setup

--- wasmlib/build.sh
+++ wasmlib/build.sh
@@ -2,4 +2,5 @@
 emcc -I../ libjseval.c -c
 emar rcs libjseval.a libjseval.o
 emcc -g -s STANDALONE_WASM --no-entry wasmlib.c libjseval.a ../libquickjs.a -o jseval.wasm
+wasm-opt -Oz jseval.wasm -o jseval.wasm
 cp jseval.wasm ../web/compiler/

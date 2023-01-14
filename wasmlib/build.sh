c47750068a98c279b09c1f0ef2385cae2fca597a
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-01-14 21:51:18 +0100

    smaller wasm lib

--- wasmlib/build.sh
+++ wasmlib/build.sh
@@ -1,6 +1,6 @@
-(cd .. && make libquickjs.a)
-emcc -I../ libjseval.c -c
+(cd .. && make clean && make libquickjs.a)
+emcc -Oz -I../ libjseval.c -c
 emar rcs libjseval.a libjseval.o
-emcc -g -s STANDALONE_WASM --no-entry wasmlib.c libjseval.a ../libquickjs.a -o jseval.wasm
+emcc -s STANDALONE_WASM --no-entry wasmlib.c libjseval.a ../libquickjs.a -Oz -o jseval.wasm
 wasm-opt -Oz jseval.wasm -o jseval.wasm
 cp jseval.wasm ../web/compiler/

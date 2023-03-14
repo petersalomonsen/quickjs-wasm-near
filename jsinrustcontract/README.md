8121931923b83dadd48b49393387fd453b22023d
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-03-14 04:52:41 +0100

    ask_ai contract method

---
+++ jsinrustcontract/README.md
@@ -0,0 +1,9 @@
+# Web4 contract for jsinrust.near.page
+
+## Building
+
+see [build.sh](build.sh)
+
+## Deploy the contract
+
+`NEAR_ENV=mainnet near deploy jsinrust.near target/wasm32-unknown-unknown/release/jsinrustcontract.wasm`
--- jsinrustcontract/src/lib.rs
+++ jsinrustcontract/src/lib.rs
@@ -12,9 +12,14 @@ impl JSInRust {
     pub fn web4_get(&self, request: Web4Request) -> Web4Response {
         return match request.path.as_str() {
             "/serviceworker.js" => Web4Response::BodyUrl { body_url: ("https://ipfs.web4.near.page/ipfs/bafkreib7supsnm6kre7yk7r7suxsjlq6etqce4zid37idjz2jsxjvmipsm?filename=serviceworker.js".to_string()) },
-            "/app.7826418e.js" => Web4Response::BodyUrl { body_url: ("https://ipfs.web4.near.page/ipfs/bafkreihwzho5mq6mngtpwfkbii7pokgoikjljwwbxnpgayxcorerqyxccy?filename=app.7826418e.js".to_string()) },
-            _ => Web4Response::BodyUrl { body_url: ("https://ipfs.web4.near.page/ipfs/bafkreihtsukks4yvtcpm5t3mrec2ccpnwwjn7jrkcu4e5quukifv7vazhu?filename=index.html".to_string()) }
+            "/app.74981da3.js" => Web4Response::BodyUrl { body_url: ("https://ipfs.web4.near.page/ipfs/bafkreibuiys7xsj46l7abnwxs3ybrwtd2swnoxujetrpdcy64eak7forhi?filename=app.74981da3.js".to_string()) },
+            _ => Web4Response::BodyUrl { body_url: ("https://ipfs.web4.near.page/ipfs/bafkreicyry3mvjevuvkxfbc7yi3cajmkmynoeujv2usobem2rxcwpk46da?filename=index.html".to_string()) }
         };
+    }
+
+    #[payable]
+    pub fn ask_ai(&mut self) {
+
     }
 
     pub fn fs_store(&mut self) {

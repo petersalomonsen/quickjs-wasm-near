2dc782f5a753962e69437eebf77b472b8e0e5aa3
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-01-14 12:06:30 +0000

    must export malloc

--- wasmlib/build.sh
+++ wasmlib/build.sh
@@ -3,6 +3,6 @@ export CC=emcc
 (cd $QUICKJS_ROOT && make clean && make CC=emcc AR=emar libquickjs.a)
 emcc -Oz -I$QUICKJS_ROOT libjseval.c -c
 emar rcs libjseval.a libjseval.o
-emcc -s STANDALONE_WASM --no-entry wasmlib.c libjseval.a $QUICKJS_ROOT/libquickjs.a -Oz -o jseval.wasm
+emcc -s STANDALONE_WASM -s EXPORTED_FUNCTIONS="['_malloc']" --no-entry wasmlib.c libjseval.a $QUICKJS_ROOT/libquickjs.a -Oz -o jseval.wasm
 wasm-opt -Oz jseval.wasm -o jseval.wasm
 cp jseval.wasm ../web/compiler/
